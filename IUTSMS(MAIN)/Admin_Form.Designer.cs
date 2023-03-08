eb519081f1af2fac90d5950b1d18d00663a11edb
ONI <rowshan@iut-dhaka.edu>
2023-03-08 22:48:21 +0600

    First Commit

---
+++ IUTSMS(MAIN)/Admin_Form.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace IUTSMS_MAIN_
+{
+    partial class Admin_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.gunaControlBox2 = new Guna.UI.WinForms.GunaControlBox();
+            this.gunaControlBox1 = new Guna.UI.WinForms.GunaControlBox();
+            this.login_AF__user_control1 = new IUTSMS_MAIN_.login_AF__user_control();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.login_AF__user_control1);
+            this.panel1.Location = new System.Drawing.Point(0, 36);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1575, 839);
+            this.panel1.TabIndex = 0;
+            // 
+            // gunaControlBox2
+            // 
+            this.gunaControlBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.gunaControlBox2.Animated = true;
+            this.gunaControlBox2.AnimationHoverSpeed = 0.07F;
+            this.gunaControlBox2.AnimationSpeed = 0.03F;
+            this.gunaControlBox2.ControlBoxType = Guna.UI.WinForms.FormControlBoxType.MinimizeBox;
+            this.gunaControlBox2.IconColor = System.Drawing.Color.Black;
+            this.gunaControlBox2.IconSize = 15F;
+            this.gunaControlBox2.Location = new System.Drawing.Point(1489, -4);
+            this.gunaControlBox2.Name = "gunaControlBox2";
+            this.gunaControlBox2.OnHoverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(52)))), ((int)(((byte)(51)))));
+            this.gunaControlBox2.OnHoverIconColor = System.Drawing.Color.White;
+            this.gunaControlBox2.OnPressedColor = System.Drawing.Color.Black;
+            this.gunaControlBox2.Size = new System.Drawing.Size(47, 43);
+            this.gunaControlBox2.TabIndex = 6;
+            // 
+            // gunaControlBox1
+            // 
+            this.gunaControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.gunaControlBox1.Animated = true;
+            this.gunaControlBox1.AnimationHoverSpeed = 0.07F;
+            this.gunaControlBox1.AnimationSpeed = 0.03F;
+            this.gunaControlBox1.IconColor = System.Drawing.Color.Black;
+            this.gunaControlBox1.IconSize = 15F;
+            this.gunaControlBox1.Location = new System.Drawing.Point(1536, -4);
+            this.gunaControlBox1.Name = "gunaControlBox1";
+            this.gunaControlBox1.OnHoverBackColor = System.Drawing.Color.Red;
+            this.gunaControlBox1.OnHoverIconColor = System.Drawing.Color.White;
+            this.gunaControlBox1.OnPressedColor = System.Drawing.Color.Black;
+            this.gunaControlBox1.Size = new System.Drawing.Size(41, 43);
+            this.gunaControlBox1.TabIndex = 5;
+            // 
+            // login_AF__user_control1
+            // 
+            this.login_AF__user_control1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.login_AF__user_control1.Location = new System.Drawing.Point(0, 0);
+            this.login_AF__user_control1.Name = "login_AF__user_control1";
+            this.login_AF__user_control1.Size = new System.Drawing.Size(1577, 838);
+            this.login_AF__user_control1.TabIndex = 0;
+            // 
+            // Admin_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1576, 874);
+            this.Controls.Add(this.gunaControlBox2);
+            this.Controls.Add(this.gunaControlBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Admin_Form";
+            this.Text = "Admin_Form";
+            this.Load += new System.EventHandler(this.Admin_Form_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Admin_Form_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Admin_Form_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Admin_Form_MouseUp);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Panel panel1;
+        private Guna.UI.WinForms.GunaControlBox gunaControlBox2;
+        private Guna.UI.WinForms.GunaControlBox gunaControlBox1;
+        private login_AF__user_control login_AF__user_control1;
+    }
+}
---
+++ IUTSMS(MAIN)/Admin_Form.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IUTSMS_MAIN_
+{
+    public partial class Admin_Form : Form
+    {
+        public Admin_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Admin_Form_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private bool dragg = false;
+        private Point StartPoint = new Point(0, 0);
+
+        private void Admin_Form_MouseDown(object sender, MouseEventArgs e)
+        {
+            dragg = true;
+            StartPoint = new Point(e.X, e.Y);
+
+
+        }
+
+        private void Admin_Form_MouseUp(object sender, MouseEventArgs e)
+        {
+            dragg = false;
+        }
+
+        private void Admin_Form_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (dragg)
+            {
+
+                Point p = PointToScreen(e.Location);
+
+                Location = new Point(p.X - this.StartPoint.X, p.Y - this.StartPoint.Y);
+
+            }
+        }
+    }
+}
