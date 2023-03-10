e45d2436c20594ad29db1424d519b1f114f1b4ea
ONI <rowshan@iut-dhaka.edu>
2023-03-10 10:04:43 +0600

    made some significant changes

--- IUTSMS(MAIN)/Student_user_control.Designer.cs
+++ IUTSMS(MAIN)/Student_user_control.Designer.cs
@@ -28,18 +28,289 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.st_cntrl_reg_panel = new System.Windows.Forms.Panel();
+            this.gunaComboBox1 = new Guna.UI.WinForms.GunaComboBox();
+            this.gunaLabel3 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaTextBox1 = new Guna.UI.WinForms.GunaTextBox();
+            this.gunaLabel2 = new Guna.UI.WinForms.GunaLabel();
+            this.streg_name_TextBox = new Guna.UI.WinForms.GunaTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.gunaLabel1 = new Guna.UI.WinForms.GunaLabel();
+            this.st_register_button = new Guna.UI.WinForms.GunaGradientTileButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.game_dev_CheckBox = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.st_cntrl_reg_panel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // st_cntrl_reg_panel
+            // 
+            this.st_cntrl_reg_panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(52)))), ((int)(((byte)(51)))));
+            this.st_cntrl_reg_panel.Controls.Add(this.st_register_button);
+            this.st_cntrl_reg_panel.Controls.Add(this.gunaComboBox1);
+            this.st_cntrl_reg_panel.Controls.Add(this.gunaLabel3);
+            this.st_cntrl_reg_panel.Controls.Add(this.gunaTextBox1);
+            this.st_cntrl_reg_panel.Controls.Add(this.gunaLabel2);
+            this.st_cntrl_reg_panel.Controls.Add(this.streg_name_TextBox);
+            this.st_cntrl_reg_panel.Controls.Add(this.label3);
+            this.st_cntrl_reg_panel.Controls.Add(this.gunaLabel1);
+            this.st_cntrl_reg_panel.Location = new System.Drawing.Point(1, 0);
+            this.st_cntrl_reg_panel.Name = "st_cntrl_reg_panel";
+            this.st_cntrl_reg_panel.Size = new System.Drawing.Size(763, 887);
+            this.st_cntrl_reg_panel.TabIndex = 0;
+            // 
+            // gunaComboBox1
+            // 
+            this.gunaComboBox1.BackColor = System.Drawing.Color.Transparent;
+            this.gunaComboBox1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.gunaComboBox1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(52)))), ((int)(((byte)(51)))));
+            this.gunaComboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.gunaComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.gunaComboBox1.FocusedColor = System.Drawing.Color.Empty;
+            this.gunaComboBox1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaComboBox1.ForeColor = System.Drawing.Color.Black;
+            this.gunaComboBox1.FormattingEnabled = true;
+            this.gunaComboBox1.Items.AddRange(new object[] {
+            "CSE",
+            "EEE",
+            "ME",
+            "CIVIL",
+            "BTM"});
+            this.gunaComboBox1.Location = new System.Drawing.Point(236, 408);
+            this.gunaComboBox1.Name = "gunaComboBox1";
+            this.gunaComboBox1.OnHoverItemBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(52)))), ((int)(((byte)(51)))));
+            this.gunaComboBox1.OnHoverItemForeColor = System.Drawing.Color.White;
+            this.gunaComboBox1.Radius = 10;
+            this.gunaComboBox1.Size = new System.Drawing.Size(377, 39);
+            this.gunaComboBox1.TabIndex = 16;
+            // 
+            // gunaLabel3
+            // 
+            this.gunaLabel3.AutoSize = true;
+            this.gunaLabel3.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaLabel3.ForeColor = System.Drawing.SystemColors.Control;
+            this.gunaLabel3.Location = new System.Drawing.Point(33, 405);
+            this.gunaLabel3.Name = "gunaLabel3";
+            this.gunaLabel3.Size = new System.Drawing.Size(171, 38);
+            this.gunaLabel3.TabIndex = 11;
+            this.gunaLabel3.Text = "Department:";
+            this.gunaLabel3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gunaTextBox1
+            // 
+            this.gunaTextBox1.BackColor = System.Drawing.Color.Transparent;
+            this.gunaTextBox1.BaseColor = System.Drawing.SystemColors.Control;
+            this.gunaTextBox1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(75)))), ((int)(((byte)(73)))));
+            this.gunaTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.gunaTextBox1.FocusedBaseColor = System.Drawing.SystemColors.Control;
+            this.gunaTextBox1.FocusedBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.gunaTextBox1.FocusedForeColor = System.Drawing.SystemColors.ControlText;
+            this.gunaTextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gunaTextBox1.Location = new System.Drawing.Point(236, 336);
+            this.gunaTextBox1.Name = "gunaTextBox1";
+            this.gunaTextBox1.PasswordChar = '\0';
+            this.gunaTextBox1.Radius = 10;
+            this.gunaTextBox1.SelectedText = "";
+            this.gunaTextBox1.Size = new System.Drawing.Size(377, 36);
+            this.gunaTextBox1.TabIndex = 8;
+            // 
+            // gunaLabel2
+            // 
+            this.gunaLabel2.AutoSize = true;
+            this.gunaLabel2.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaLabel2.ForeColor = System.Drawing.SystemColors.Control;
+            this.gunaLabel2.Location = new System.Drawing.Point(154, 334);
+            this.gunaLabel2.Name = "gunaLabel2";
+            this.gunaLabel2.Size = new System.Drawing.Size(50, 38);
+            this.gunaLabel2.TabIndex = 9;
+            this.gunaLabel2.Text = "ID:";
+            this.gunaLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // streg_name_TextBox
+            // 
+            this.streg_name_TextBox.BackColor = System.Drawing.Color.Transparent;
+            this.streg_name_TextBox.BaseColor = System.Drawing.SystemColors.Control;
+            this.streg_name_TextBox.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(75)))), ((int)(((byte)(73)))));
+            this.streg_name_TextBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.streg_name_TextBox.FocusedBaseColor = System.Drawing.SystemColors.Control;
+            this.streg_name_TextBox.FocusedBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.streg_name_TextBox.FocusedForeColor = System.Drawing.SystemColors.ControlText;
+            this.streg_name_TextBox.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.streg_name_TextBox.Location = new System.Drawing.Point(236, 261);
+            this.streg_name_TextBox.Name = "streg_name_TextBox";
+            this.streg_name_TextBox.PasswordChar = '\0';
+            this.streg_name_TextBox.Radius = 10;
+            this.streg_name_TextBox.SelectedText = "";
+            this.streg_name_TextBox.Size = new System.Drawing.Size(377, 36);
+            this.streg_name_TextBox.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.label3.Location = new System.Drawing.Point(73, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(678, 108);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Please register here using Student ID\r\n\r\n";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gunaLabel1
+            // 
+            this.gunaLabel1.AutoSize = true;
+            this.gunaLabel1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaLabel1.ForeColor = System.Drawing.SystemColors.Control;
+            this.gunaLabel1.Location = new System.Drawing.Point(115, 259);
+            this.gunaLabel1.Name = "gunaLabel1";
+            this.gunaLabel1.Size = new System.Drawing.Size(113, 38);
+            this.gunaLabel1.TabIndex = 7;
+            this.gunaLabel1.Text = "Name:  ";
+            this.gunaLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // st_register_button
+            // 
+            this.st_register_button.Animated = true;
+            this.st_register_button.AnimationHoverSpeed = 0.07F;
+            this.st_register_button.AnimationSpeed = 0.03F;
+            this.st_register_button.BackColor = System.Drawing.Color.Transparent;
+            this.st_register_button.BaseColor1 = System.Drawing.SystemColors.Control;
+            this.st_register_button.BaseColor2 = System.Drawing.Color.PapayaWhip;
+            this.st_register_button.BorderColor = System.Drawing.Color.Black;
+            this.st_register_button.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.st_register_button.FocusedColor = System.Drawing.Color.Empty;
+            this.st_register_button.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.st_register_button.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(52)))), ((int)(((byte)(51)))));
+            this.st_register_button.Image = null;
+            this.st_register_button.ImageSize = new System.Drawing.Size(52, 52);
+            this.st_register_button.Location = new System.Drawing.Point(236, 512);
+            this.st_register_button.Name = "st_register_button";
+            this.st_register_button.OnHoverBaseColor1 = System.Drawing.Color.PapayaWhip;
+            this.st_register_button.OnHoverBaseColor2 = System.Drawing.SystemColors.Control;
+            this.st_register_button.OnHoverBorderColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.st_register_button.OnHoverForeColor = System.Drawing.Color.Black;
+            this.st_register_button.OnHoverImage = null;
+            this.st_register_button.OnPressedColor = System.Drawing.Color.Black;
+            this.st_register_button.Radius = 20;
+            this.st_register_button.Size = new System.Drawing.Size(377, 57);
+            this.st_register_button.TabIndex = 12;
+            this.st_register_button.Text = "Register";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(52)))), ((int)(((byte)(51)))));
+            this.label1.Location = new System.Drawing.Point(906, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(451, 54);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "And select your hobbies";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(52)))), ((int)(((byte)(51)))));
+            this.label2.Location = new System.Drawing.Point(840, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(613, 28);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Our software will suggest you suitable society based on your hobbies";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // game_dev_CheckBox
+            // 
+            this.game_dev_CheckBox.AutoSize = true;
+            this.game_dev_CheckBox.BackColor = System.Drawing.SystemColors.Control;
+            this.game_dev_CheckBox.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.game_dev_CheckBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(52)))), ((int)(((byte)(51)))));
+            this.game_dev_CheckBox.Location = new System.Drawing.Point(845, 189);
+            this.game_dev_CheckBox.Name = "game_dev_CheckBox";
+            this.game_dev_CheckBox.Size = new System.Drawing.Size(159, 32);
+            this.game_dev_CheckBox.TabIndex = 19;
+            this.game_dev_CheckBox.Text = "Programming";
+            this.game_dev_CheckBox.UseVisualStyleBackColor = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.checkBox1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(52)))), ((int)(((byte)(51)))));
+            this.checkBox1.Location = new System.Drawing.Point(845, 227);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(156, 60);
+            this.checkBox1.TabIndex = 20;
+            this.checkBox1.Text = "Web Design & \r\nDevlopment";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.checkBox2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(52)))), ((int)(((byte)(51)))));
+            this.checkBox2.Location = new System.Drawing.Point(845, 293);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(105, 32);
+            this.checkBox2.TabIndex = 21;
+            this.checkBox2.Text = "Gaming";
+            this.checkBox2.UseVisualStyleBackColor = false;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.BackColor = System.Drawing.SystemColors.Control;
+            this.checkBox3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(52)))), ((int)(((byte)(51)))));
+            this.checkBox3.Location = new System.Drawing.Point(845, 341);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(205, 75);
+            this.checkBox3.TabIndex = 22;
+            this.checkBox3.Text = "Cybersecurity-\r\nHacking";
+            this.checkBox3.UseVisualStyleBackColor = false;
             // 
             // Student_user_control
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.game_dev_CheckBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.st_cntrl_reg_panel);
             this.Name = "Student_user_control";
             this.Size = new System.Drawing.Size(1480, 888);
+            this.st_cntrl_reg_panel.ResumeLayout(false);
+            this.st_cntrl_reg_panel.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel st_cntrl_reg_panel;
+        private Guna.UI.WinForms.GunaLabel gunaLabel3;
+        private Guna.UI.WinForms.GunaTextBox gunaTextBox1;
+        private Guna.UI.WinForms.GunaLabel gunaLabel2;
+        private Guna.UI.WinForms.GunaTextBox streg_name_TextBox;
+        private System.Windows.Forms.Label label3;
+        private Guna.UI.WinForms.GunaLabel gunaLabel1;
+        private Guna.UI.WinForms.GunaComboBox gunaComboBox1;
+        private Guna.UI.WinForms.GunaGradientTileButton st_register_button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox game_dev_CheckBox;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
     }
 }
